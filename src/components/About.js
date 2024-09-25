27d0f453951d07754d4f2a12106c19f157c779b0
karlinamaya <karli.maya12@gmail.com>
2024-09-25 08:32:23 -0400

    Updated social links

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import '../styles/About.css';
 
 const About = () => {
   return (
@@ -14,10 +16,18 @@ const About = () => {
           'researcher 🔎',
           1100,
         ]}
-        speed={50}
+        speed={53}
         style={{ fontSize: '1em', display: 'inline-block' }}
         repeat={Infinity}
       /></p>
+      <div className="social-icons">
+        <a href="https://github.com/karlinamaya" target="_blank" rel="noopener noreferrer">
+          <FaGithub style={{ fontSize: 23, color: 'white', margin: '0 10px' }} />
+        </a>
+        <a href="https://www.linkedin.com/in/karlina-maya/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin style={{ fontSize: 23, color: 'white', margin: '0 10px' }} />
+        </a>
+      </div>
     </section>
   );
 };
