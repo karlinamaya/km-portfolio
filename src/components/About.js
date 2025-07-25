8ab04a82cbaa704ef4632d8e2e09676ee9605c5c
Karlina <107602124+karlinamaya@users.noreply.github.com>
2025-07-25 10:07:09 -0400

    Updated portfolio content

--- src/components/About.js
+++ src/components/About.js
@@ -20,24 +20,22 @@ const About = () => {
   return (
     <section id="about">
       <div className="about-container">
-        <h1>Hi, I'm Karlina</h1>
-        <p>I'm a <TypeAnimation
-          sequence={[
-            'designer 🎨',
-            1100,
-            'developer 💻',
-            1100,
-            'researcher 🔎',
-            1100,
-          ]}
-          speed={53}
-          style={{ fontSize: '1em', display: 'inline-block' }}
-          repeat={Infinity}
-        /></p>
+        <h1>
+          <TypeAnimation
+            sequence={[
+              'Hi, I\'m Karlina 👋', 
+              2000,
+            ]}
+            speed={50}
+            style={{ fontSize: '1em', display: 'inline-block' }}
+          />
+        </h1>
+        <div className="ux-designer-tagline">
+        <p>
+          I'm a UX Designer with a deep passion for creating <span className="gradient-text">intuitive</span> and <span className="gradient-text">accessible</span> user experiences.
+        </p>
+        </div>
         <div className="social-icons">
-          <a href="https://github.com/karlinamaya" target="_blank" rel="noopener noreferrer">
-            <FaGithub style={{ fontSize: 23, color: 'white', margin: '0 10px' }} />
-          </a>
           <a href="https://www.linkedin.com/in/karlina-maya/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin style={{ fontSize: 23, color: 'white', margin: '0 10px' }} />
           </a>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,18 +8,6 @@ const Projects = () => {
       <h1>Projects</h1>
       <div className="project-grid">
         <div className="project">
-          <a href="https://github.com/karlinamaya/connect-four" target="_blank" rel="noreferrer noopener">
-            <img src="/images/connectfour.png" alt="Connect Four" />
-          </a>
-          <h2>Connect Four</h2>
-          <p>Developed a web-based Connect Four game with interactive gameplay</p>
-          <div className="tech-buttons">
-            <span className="tech-button">javascript</span>
-            <span className="tech-button">html</span>
-            <span className="tech-button">bootstrap css</span>
-          </div>
-        </div>
-        <div className="project">
           <Link to="/snout">
             <img src="/images/snout.png" alt="Snout" />
           </Link>
@@ -33,7 +21,20 @@ const Projects = () => {
           </div>
         </div>
         <div className="project">
-          <a href="https://github.com/karlinamaya/wildlife-tracker" target="_blank" rel="noreferrer noopener">
+          {/* <a href="https://github.com/karlinamaya/connect-four" target="_blank" rel="noreferrer noopener"> */}
+          <a href="https://karlinamaya.github.io/connect-four/" target="_blank" rel="noreferrer noopener">
+            <img src="/images/connectfour.png" alt="Connect Four" />
+          </a>
+          <h2>Connect Four</h2>
+          <p>Developed a web-based Connect Four game with interactive gameplay</p>
+          <div className="tech-buttons">
+            <span className="tech-button">javascript</span>
+            <span className="tech-button">html</span>
+            <span className="tech-button">bootstrap css</span>
+          </div>
+        </div>
+        <div className="project">
+          <a href="https://devpost.com/software/knights-wildlife-tracker" target="_blank" rel="noreferrer noopener">
             <img src="/images/wildlife-tracker.png" alt="Wildlife Tracker" />
           </a>
           <h2>Wildlife Tracker</h2>
@@ -46,7 +47,7 @@ const Projects = () => {
             <span className="tech-button">mongodb</span>
           </div>
         </div>
-        <div className="project">
+        {/* <div className="project">
           <a href="https://github.com/karlinamaya/AmplifyHER" target="_blank" rel="noreferrer noopener">
             <img src="/images/amplifyher.png" alt="AmplifyHER" />
           </a>
@@ -58,7 +59,7 @@ const Projects = () => {
             <span className="tech-button">bootstrap css</span>
             <span className="tech-button">spotify api</span>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
